3af12d28fd8eb95ca75ffe14337b326b5c997685
Hoaipt1998 <54972641+Hoaipt1998@users.noreply.github.com>
2022-02-19 17:50:25 +0700

    finish

---
+++ app/src/main/java/com/android30112021/thread/MainActivity.java
@@ -0,0 +1,164 @@
+package com.android30112021.thread;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int a, b, c;
+    MyFlag myFlag;
+    Handler handler;
+    TextView mTvA, mTvB, mTvC;
+
+    @SuppressLint("HandlerLeak")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTvA = findViewById(R.id.textA);
+        mTvB = findViewById(R.id.textB);
+        mTvC = findViewById(R.id.textC);
+
+        a = b = c = 0;
+        myFlag = new MyFlag(0);
+
+        // count
+        // 0 => in ra a
+        // 1 => in ra b
+        // 2 => in ra c
+        handler = new Handler() {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what) {
+                    case 0:
+                        mTvA.setText("A " + msg.obj);
+                        break;
+                    case 1:
+                        mTvB.setText("B " + msg.obj);
+                        break;
+                    case 2:
+                        mTvC.setText("C " + msg.obj);
+                        break;
+                }
+            }
+        };
+
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (myFlag) {
+                    for (int i = 0; i < 10; ) {
+                        if (myFlag.count == 0) {
+                            a = i;
+                            try {
+                                Thread.sleep(200);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            Message message = new Message();
+                            message.what = 0;
+                            message.obj = a;
+                            handler.sendMessage(message);
+                            myFlag.count = 1;
+                            myFlag.notifyAll();
+                            i++;
+                        } else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (myFlag) {
+                    for (int i = 0; i < 10; ) {
+                        if (myFlag.count == 1) {
+                            b = i;
+                            try {
+                                Thread.sleep(200);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            Message message = new Message();
+                            message.what = 1;
+                            message.obj = b;
+                            handler.sendMessage(message);
+                            myFlag.count = 2;
+                            myFlag.notifyAll();
+                            i++;
+                        } else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (myFlag) {
+                    for (int i = 0; i < 10; ) {
+                        if (myFlag.count == 2) {
+                            c = a + b;
+                            try {
+                                Thread.sleep(200);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            Message message = new Message();
+                            message.what = 2;
+                            message.obj = c;
+                            handler.sendMessage(message);
+                            myFlag.count = 0;
+                            myFlag.notifyAll();
+                            i++;
+                        } else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+    }
+        private synchronized void printlog (String text)
+        {
+            for (int i = 0; i < 50; i++) {
+                Log.d("BBB", text + " : " + i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
---
+++ app/src/main/java/com/android30112021/thread/MyFlag.java
@@ -0,0 +1,11 @@
+package com.android30112021.thread;
+
+public class MyFlag {
+    int count;
+
+
+    public  MyFlag(int count)
+    {
+        this.count = count;
+    }
+}
